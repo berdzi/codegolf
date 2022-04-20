2eeae3c463535bf55b6cf66d104cd5ba866d9b71
berdzi <berdzii@o2.pl>
2022-04-20 10:05:52 +0200

    rock papers scissors spock lizard

---
+++ Rock-paper-scissors-Spock-lizard/Rock-paper-scissors-Spock-lizard_377.py
@@ -0,0 +1,9 @@
+#377
+import sys
+p=print
+s={"✂📄":"cut","📄💎":"cover","💎🦎":"crushe","🦎🖖":"poison","🖖✂":"smashe","✂🦎":"decapitate","🦎📄":"eat","📄🖖":"disprove","🖖💎":"vaporize","💎✂":"crushe"}
+for a in sys.argv[1:]:
+	if a[0]==a[1]:p("Tie")
+	else:
+		if a in s.keys():p(a[0]+' '+s[a]+'s '+a[1])
+		if a[::-1] in s.keys():p(a[1]+' '+s[a[::-1]]+'s '+a[0])
